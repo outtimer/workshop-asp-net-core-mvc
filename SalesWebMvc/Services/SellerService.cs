8646c57a395ee3f857bf683bb782ecc103be2be1
OutTimer <diogo.out@outlook.com>
2019-02-24 14:38:43 -0300

    Implementing simple search

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Services.Exceptions;
-using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
